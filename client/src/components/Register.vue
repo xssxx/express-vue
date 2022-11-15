b5c5bdb6916752f33dfdb15c90c8823ccd864c37
koonx6520 <koonx6520@gmail.com>
2022-11-15 20:16:18 +0700

    add api to call backend

---
+++ client/src/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <input v-model="email" type="email" name="email" placeholder="email...">
+        <br>
+        <input v-model="password" type="password" name="password" placeholder="password...">
+        <br>
+        <button @click="register()">Register</button>
+    </div>
+</template>
+
+<script>
+import AuthenticationService from '../services/AuthenticationService'
+export default {
+    name: 'Register',
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async register() {
+            const res = await AuthenticationService.register({
+                email: this.email,
+                password: this.password
+            })
+            console.log(res)
+        }
+    }
+}
+</script>
